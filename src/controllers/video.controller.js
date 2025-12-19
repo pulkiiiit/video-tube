a84335a7df429083c348f149c4584c933c16a704
pulkiiiit <pulkitshukla001@gmail.com>
2025-12-19 10:59:05 +0530

    Implement video CRUD functionality and enhance video management features

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,32 +9,157 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
+    //TODO: get all videos based on query, sort, pagination 
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    if (!title || !description) {
+        throw new ApiError(400, "Title and description cannot be empty")
+    }
+
+    const user = await User.findById(req.user?._id)
+
+    const videoLocalPath = req.files?.videoFile[0]?.path;
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+
+    if(!videoLocalPath || !thumbnailLocalPath) {
+        throw new ApiError(400, "upload the video and thumbnail properly")
+    }
+    
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+    const videoFile = await uploadOnCloudinary(videoLocalPath);
+    
+
+
+    console.log(thumbnailLocalPath)
+
+    if (!videoFile || !thumbnail) {
+        throw new ApiError(400, "There was some error while uploding your stuff on clodinary")
+    }
+
+
+    const video = await Video.create({
+        title,
+        description,
+        videoFile : videoFile.url,
+        duration : videoFile.duration,
+        thumbnail : thumbnail.url,
+        owner : user._id
+    })
+
+    if (!video) {
+        throw new ApiError(500, "Something went wrong while registering the Video on database")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, video, "Video registerd successfully")
+    )
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+
+    const video = await Video.findById(videoId)
+
+    console.log(video)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, video, "video fetched successfully")
+    )
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    const {title , description} = req.body
+
+
+    const updateField = {}
+    if (title) {
+        updateField.title = title
+    }
+
+    if (description) {
+        updateField.description = description
+    }
+
+    if (req.file?.path) {
+        const newThumbnail = await uploadOnCloudinary(req.file.path)
+
+        if (!newThumbnail?.url) {
+            throw new ApiError(400, "Error uploading thumbnail")
+        }
+
+        updateField.thumbnail = newThumbnail.url
+    }
+
+    if (Object.keys(updateField).length === 0) {
+        throw new ApiError(400, "At least one field is requried to update")
+    }
+
+    const video = await Video.findByIdAndUpdate(
+        videoId,
+        { $set : updateField},
+        {new : true}
+    )
+
+    if (!video) {
+        throw new ApiError(404, "Cannot retrive the video while updating")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, video, "video detials updated successfully")
+    )
 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+
+    await Video.findByIdAndUpdate(videoId)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "video deleted successfully")
+    )
+
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+
+    const video = await Video.findByIdAndUpdate(
+        videoId,
+        [
+            {
+                $set:{
+                    isPublished: { $not: "$isPublished" }
+                }
+            }
+        ],
+        {new : true}
+    )
+
+    if (!video) {
+        throw new ApiError(400, "cannot fetch the video after updating the toggle status")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200 , video , "changed the publishing status successfully")
+    )
 })
 
 export {
